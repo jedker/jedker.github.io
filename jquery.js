50fcd1cb751663ab2a3f6a3165df6f69c8dc093e
jedker <Krzysztof.jedras@gmail.com>
2017-03-15 13:48:25 +0100

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,5 +1,67 @@
 var divs = $('#nav_bar');
 $(window).on('scroll', function() {
    var st = $(this).scrollTop();
-   divs.css({ 'opacity' : (1 - st/900) });
+   divs.css({ 'opacity' : (1 - st/800) });
 });
+
+var diva = $('.photo');
+$(window).on('scroll', function() {
+   var sp = $(this).scrollTop();
+   diva.css({ 'opacity' : (1 - sp/1200) });
+});
+
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+$(window).scroll(function(event) {
+  
+  $(".slide").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
+
+$("#click").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+       
+    });
+
+});
+
