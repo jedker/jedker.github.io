c805864f76dae6f6a6d697ebb23a4c793c73d557
jedker <Krzysztof.jedras@gmail.com>
2017-03-25 15:10:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $('.button2').click(function(){
 			
 			$('#drag').animate({"left": "0",
 								"width": "100%",
-								"height": "45px"}, 1000);
+								"height": "100px"}, 500);
 			$('#drag').addClass("left-bar");
 			$('.menu').removeClass("not-visible");
 			$(".menu>>li>a").addClass("menu_black");
@@ -24,7 +24,7 @@ $('.button2').click(function(){
 			$('#drag').animate({
 								"left": "+=47vw",
 								"width": "-=94.75vw",
-								"height": "100vh"},500);
+								"height": "+=100vh"},500);
 			$('#expand_me').removeClass('hidden');
 			$('#drag').removeClass("left-bar");
 			$('.menu').addClass("not-visible");
@@ -39,7 +39,7 @@ $('.button2').click(function(){
 			$
 			$('#drag').animate({"left": "0",
 								"width": "100%",
-								"height": "45px"}, 500);
+								"height": "100px"}, 500);
 			$('#drag').addClass("right-bar");
 			$('.menu').removeClass("not-visible");
 			$(".description").addClass('hidden-right');
@@ -52,7 +52,7 @@ $('.button2').click(function(){
 			$('#drag').animate({
 								"left": "+=47vw",
 								"width": "-=94.75vw",
-								"height": "100vh"},500);
+								"height": "+=100vh"},500);
 			$('.description').removeClass('hidden-right');
 			$('#drag').removeClass("right-bar");
 			$('.menu').addClass("not-visible");
